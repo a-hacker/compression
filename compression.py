ecf474c340a086c090550d27e0e3dc85f9edd924
Austin Hacker <explosivepyrounicorns@gmail.com>
2013-10-13 22:01:16 -0400

    better variable names and better comments

--- compression.py
+++ compression.py
@@ -1,84 +1,61 @@
 from linked_list import LinkedList
 from linked_list import LLNode
 
-def segment_and_store(to_be_segmented, segment_length, output_location):
+def compress(to_be_segmented, segment_length, output_location):
+	"""
+	A method to compress a file by segmenting the text then allocating an integer value to represent that segmented string
+	then optimize the compression by creating a list of positions of each segment at a given time 
+	"""
 
-	"""
-	create: 
-	    head - head of doubly linkedlist
-    	segment - to represent a section of text
-	    dictionary - to store segment as a key with a linked list node reference as value
-	    output_list - the compressed text
-	"""
-	head = LinkedList()
-	segment = ''
-	dictionary = {}
-	output_list = []
+	#Edge Case: we can't do anything if the segment_length doesn't make sense
+	if segment_length <= 0:
+		return
 
-	"""
-	legend = 0
-	"""
-	legend = 0
-	
-	"""
-	open to_be_segmented
-	"""
-	current_letter = 0
-	with open(to_be_segmented) as f:
+	position_list = LinkedList() 	#doubly linkedlist of nodes that correspond to a segment for easy reordering
+	segment = '' 					#to represent a section of text
+	initial_legend = {} 			#to store segment as a key with (corresponding LLNode reference, compression value) as value
+	output_list = [] 				#the compressed text as a list of positions of compressed values from position_list
 
-		"""
-		for each letter (letter) in to_be_segmented:
-		"""
+	compression_value = 0			#an integer to represent a segment counting from 0 up by when it was added
+	current_letter = 0				#an interger to remember where in the text the current letter is for seeking back
+	with open(to_be_segmented) as text:
+
 		while True:
-			letter = f.read(1)
+			
+			#End Case (if the current letter is the end of the file, we break the while true loop)
+			letter = text.read(1)
 			if not letter:
 				break
 
-			"""
-			add letter to segment
-			"""
 			segment = segment + letter
 
-			"""
-			if length of segment < segment_length and next is not end and next is not alphanumerical and letter is alphanumerical:
-				continue
-			"""
-			next = f.read(1)
-			f.seek(current_letter)
+			
+			next = text.read(1)
+			text.seek(current_letter)
 			current_letter += 1
+
+			
+			#check if the segment is ready to be added
 			if len(segment) < segment_length and not next and not next.isalnum() and letter.isalnum():
 				continue
 
-			"""
-			if segment in dictionary:
-				find corresponding node using segment as key in dictionary
-				add position of node to output_list and move node to front of list
-				empty segment
-			"""
-			if segment in dictionary:
-				output_list.append(dictionary[segment][0].position)
-				head.move_to_head(dictionary[segment][0])
+			
+			#if segment in dictionary: create an LLNode and add last position to output_list
+			if segment in initial_legend:
+				output_list.append(initial_legend[segment][0].position)
+				position_list.move_to_head(initial_legend[segment][0])
 				segment = ""
 
-			"""
-			if segment not in dictionary:
-				create new linkedlist node and append to the front of the list
-				add legend - 1 to output_list
-				add (segment: (node, legend)) to dictionary y
-				empty segment
-			"""
-			if segment not in dictionary:
-				x = LLNode()
-				head.add_to_head(x)
+			
+			#if segment not in dictionary: find it in head and add position to output_list
+			if segment not in initial_legend:
+				new_node = LLNode()
+				position_list.add_to_head(new_node)
 				output_list.append(legend)
 				legend += 1
-				dictionary[segment] = (x, legend)
+				initial_legend[segment] = (new_node, compression_value)
 				segment = ""
 
-	"""
-	print string object of output_list with spaces seperating numbers
-	print string of dictionary values in order
-	"""
-	d = open(output_location)
-	d.write(map(str, output_list))
-	d.write(map(str, dictionary))
+	output_file = open(output_location)
+	output_file.write(map(str, output_list)) #print string object of output_list with spaces seperating numbers
+	output_file.write(map(str, dictionary)) #print string of dictionary values in order
