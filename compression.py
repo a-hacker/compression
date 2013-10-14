e13e0e5a0b64899efe8c91db65cb23f54f915581
Austin Hacker <explosivepyrounicorns@gmail.com>
2013-10-13 20:50:53 -0400

    initial compression code and updated linked_list

--- compression.py
+++ compression.py
@@ -1,38 +1,84 @@
-"""
-void segment_and_store(file to_be_segmented, int segment_length):
+from linked_list import LinkedList
+from linked_list import LLNode
 
-	open to_be_segmented
+def segment_and_store(to_be_segmented, segment_length, output_location):
 
+	"""
 	create: 
 	    head - head of doubly linkedlist
     	segment - to represent a section of text
 	    dictionary - to store segment as a key with a linked list node reference as value
 	    output_list - the compressed text
+	"""
+	head = LinkedList()
+	segment = ''
+	dictionary = {}
+	output_list = []
 
-	if empty then
-		return an empty string
-
+	"""
 	legend = 0
-	for each letter (letter) in to_be_segmented:
-		
-		add letter to segment
+	"""
+	legend = 0
+	
+	"""
+	open to_be_segmented
+	"""
+	current_letter = 0
+	with open(to_be_segmented) as f:
 
-		if length of segment < segment_length and next is not end and next is not alphanumerical and letter is alphanumerical:
-			continue
+		"""
+		for each letter (letter) in to_be_segmented:
+		"""
+		while True:
+			letter = f.read(1)
+			if not letter:
+				break
 
-		if segment in dictionary:
-			find corresponding node using segment as key in dictionary
-			add position of node to output_list and move node to front of list
-			empty segment
+			"""
+			add letter to segment
+			"""
+			segment = segment + letter
 
-		if segment not in dictionary:
-			create new linkedlist node and append to the front of the list
-			add legend - 1 to output_list
-			add (segment: (node, legend)) to dictionary y
-			empty segment
+			"""
+			if length of segment < segment_length and next is not end and next is not alphanumerical and letter is alphanumerical:
+				continue
+			"""
+			next = f.read(1)
+			f.seek(current_letter)
+			current_letter += 1
+			if len(segment) < segment_length and not next and not next.isalnum() and letter.isalnum():
+				continue
 
-	close to_be_segmented
+			"""
+			if segment in dictionary:
+				find corresponding node using segment as key in dictionary
+				add position of node to output_list and move node to front of list
+				empty segment
+			"""
+			if segment in dictionary:
+				output_list.append(dictionary[segment][0].position)
+				head.move_to_head(dictionary[segment][0])
+				segment = ""
 
+			"""
+			if segment not in dictionary:
+				create new linkedlist node and append to the front of the list
+				add legend - 1 to output_list
+				add (segment: (node, legend)) to dictionary y
+				empty segment
+			"""
+			if segment not in dictionary:
+				x = LLNode()
+				head.add_to_head(x)
+				output_list.append(legend)
+				legend += 1
+				dictionary[segment] = (x, legend)
+				segment = ""
+
+	"""
 	print string object of output_list with spaces seperating numbers
 	print string of dictionary values in order
-"""
+	"""
+	d = open(output_location)
+	d.write(map(str, output_list))
+	d.write(map(str, dictionary))
--- linked_list.py
+++ linked_list.py
@@ -3,6 +3,7 @@ class LLNode:
 	def __init__(self, prev=None, next=None):
 		self.previous = prev
 		self.next = next
+		self.position = 0
 
 class LinkedList:
 
@@ -10,17 +11,28 @@ class LinkedList:
 		self.head = head
 		self.tail = tail
 
+	def update_positions(self, node=self.tail):
+		if node == head:
+			node.position = 0
+		else:
+			self.update_positions(node.previous)
+			position = node.previous.position + 1
+
 	def add_to_head(self, node):
+		node.position = 0
 		if self.head == None:
 			self.head = node
+			self.tail = node
 		else:
 			self.head.previous = node
 			self.head = node
+		self.update_positions()
 
 	def move_to_head(self, node):
 		if self.head == node:
 			return
 		if node == self.tail:
+			self.tail = node.previous
 			node.previous.next = None
 			node.next = self.head
 			node.previous = None
@@ -31,4 +43,5 @@ class LinkedList:
 			node.next = self.head
 			node.previous = None
 			self.head = node
+		self.update_positions()
 
